1b06d5ab6713189a05317f53ab19e0b5b9cf6672
mvadher <97686301+mvadher@users.noreply.github.com>
2024-11-03 09:37:08 -0500

    Update hero.tsx

--- src/features/ui/hero.tsx
+++ src/features/ui/hero.tsx
@@ -11,10 +11,10 @@ export const Hero: FC<HeroProps> = (props) => {
     <div className="border-b w-full py-4">
       <div className="container max-w-4xl h-full flex flex-col gap-4">
         <div className="flex gap-6 flex-col items-start">
-          <h1 className="text-6xl font-bold flex gap-2 items-center">
+          <h1 className="text-5xl font-bold flex gap-2 items-center">
             {props.title}
           </h1>
-          <p className="text-muted-foreground max-w-xl">{props.description}</p>
+          <p className="text-muted-foreground max-w-4xl">{props.description}</p>
         </div>
         <div className="grid grid-cols-3 gap-2">{props.children}</div>
       </div>
