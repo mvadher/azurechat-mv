5d2bf02dcfa1f61aac24f44f24c5218f5d169a96
mvadher <97686301+mvadher@users.noreply.github.com>
2025-04-19 05:29:10 -0400

    login.tsx

--- src/features/auth-page/login.tsx
+++ src/features/auth-page/login.tsx
@@ -29,7 +29,7 @@ export const LogIn: FC<LoginProps> = (props) => {
           <span className="text-primary">{AI_NAME}</span>
         </CardTitle>
         <CardDescription>
-          Login in with your GitHub or Microsoft 365 account
+          Private GPT for Alternative Investments
         </CardDescription>
       </CardHeader>
       <CardContent className="grid gap-4">
