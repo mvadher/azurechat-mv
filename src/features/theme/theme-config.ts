50c9588ba763fd741983d8b961ee2150f33878e4
mvadher <97686301+mvadher@users.noreply.github.com>
2025-04-19 04:04:01 -0400

    theme-config.ts

--- src/features/theme/theme-config.ts
+++ src/features/theme/theme-config.ts
@@ -1,8 +1,8 @@
 export const AI_NAME = "Cohesive360 AI";
-export const AI_DESCRIPTION = "Experience secure, private AI solutions designed to transform data into valuable insights for your business. At Cohesive360, your data privacy is our priority—analyze, generate, and manage insights with confidence on our trusted platform.";
+export const AI_DESCRIPTION = "Experience secure, private AI solutions designed to transform data into valuable insights for your business.";
 export const CHAT_DEFAULT_PERSONA = AI_NAME + " default";
 
-export const CHAT_DEFAULT_SYSTEM_PROMPT = `You are a friendly ${AI_NAME} AI assistant. You must always return in markdown format.
+export const CHAT_DEFAULT_SYSTEM_PROMPT = `You are an alternative investment industry ${AI_NAME} assistant. You must always return in markdown format.
 
 You have access to the following functions:
 1. create_img: You must only use the function create_img if the user asks you to create an image.`;
