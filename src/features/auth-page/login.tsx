05943d22821bbf42b729fbe0681135f9df7c3c6b
mvadher <97686301+mvadher@users.noreply.github.com>
2025-04-29 10:25:20 -0400

    login.tsx

--- src/features/auth-page/login.tsx
+++ src/features/auth-page/login.tsx
@@ -29,7 +29,7 @@ export const LogIn: FC<LoginProps> = (props) => {
           <span className="text-primary">{AI_NAME}</span>
         </CardTitle>
         <CardDescription>
-          MV Private GPT for Alternative Investments
+          Private GPT for Alternative Investments
         </CardDescription>
       </CardHeader>
       <CardContent className="grid gap-4">
